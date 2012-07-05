8d9d2f49afd5bbfe2401b782304d9018ce1a96e4
alexclp <alexandru.clapa@gmail.com>
2012-07-05 09:44:43 +0300

    Initial Commit

---
+++ Stiri/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Stiri
+//
+//  Created by Alex Clapa on 05.07.2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
